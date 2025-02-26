007335c8921ac7b3005f26ebf6f24cfdf9bbb934
André Pilco <andrepilcoch@gmail.com>
2025-02-25 22:40:44 -0500

    additional info

--- JuicyMusic.Data/obj/Debug/net9.0/JuicyMusic.Data.AssemblyInfo.cs
+++ JuicyMusic.Data/obj/Debug/net9.0/JuicyMusic.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JuicyMusic.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255e56a8c231805e42377f0ee162a468aeafd7fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d2d19a883dedf292925116a41c403c675a07a15")]
 [assembly: System.Reflection.AssemblyProductAttribute("JuicyMusic.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JuicyMusic.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
