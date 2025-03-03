392dac3f6658a66f6f3ca988761f208467662d4e
André Pilco <andrepilcoch@gmail.com>
2025-03-02 21:37:38 -0500

    createalbumcommand

--- JuicyMusic.Application/Commands/Albums/CreateAlbum/CreateAlbumCommand.cs
+++ JuicyMusic.Application/Commands/Albums/CreateAlbum/CreateAlbumCommand.cs
@@ -5,5 +5,19 @@ namespace JuicyMusic.Application.Commands.Albums;
 
 public record CreateAlbumCommand : IRequest<AlbumResponseDto>
 {
+    public string Name { get; set; }
 
+    public int TypeId { get; set; }
+
+    public int TotalTracks { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+
+    public int DurationMs { get; set; }
+
+    public Guid GenreId { get; set; }
+
+    public Guid ArtistId { get; set; }
+
+    public string ImageUrl { get; set; }
 }
